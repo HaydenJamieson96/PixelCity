a83cac7a8ba5f56b0ac0564818dae0fc97a3a702
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-04 10:49:04 +0000

    Project Initial Config
    Alamofire installed
    Project setup

---
+++ PixelCity/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PixelCity
+//
+//  Created by Hayden Jamieson on 04/01/2018.
+//  Copyright © 2018 Hayden Jamieson. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-PixelCityUITests/Pods-PixelCityUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PixelCityUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PixelCityUITests
+@end
