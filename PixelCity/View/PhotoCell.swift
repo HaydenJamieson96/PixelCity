a8ec731f25af9d98283c7ad0df3bb97593b80b34
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-04 12:56:25 +0000

    Configured collection view

---
+++ PixelCity/View/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  PixelCity
+//
+//  Created by Hayden Jamieson on 04/01/2018.
+//  Copyright © 2018 Hayden Jamieson. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
